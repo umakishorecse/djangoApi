7c13dc308b876a744d1487d947c6c446442ac97d
kishore <kishore99855@gmail.com>
2019-03-10 22:52:11 +0530

    myData error solved

--- src/app/records.service.ts
+++ src/app/records.service.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@angular/core';
 import {HttpClient } from '@angular/common/http';
 
-
-
 @Injectable({
   providedIn: 'root'
 })
 export class RecordsService {
 
   constructor(private http:HttpClient) { }
-    getDatas(){
-      return this.http.get<myData>('http://localhost:8000/employees/')
+    getDatas() {
+      return this.http.get<any[]>('http://localhost:8000/employees/')
       
     }
 
